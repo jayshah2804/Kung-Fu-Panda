9e3e7ffa42fc47169fa02cd44937492b06b68708
jayshah2804 <jayshah9791@gmail.com>
2022-07-02 18:00:35 +0530

    a

--- src/components/Movie/MovieDetail.js
+++ src/components/Movie/MovieDetail.js
@@ -40,18 +40,32 @@ export const MovieDetail = () => {
               {data.name}
             </h3>
             <div className={classes.description}>
-              <p>
-                <span>Director: </span>
-                {data.director}
-              </p>
-              <p>
-                <span>Casts: </span>
-                {data.casts}
-              </p>
-              <p>
-                <span>Genre: </span>
-                {data.genre}
-              </p>
+              <div className="row">
+                <div className="col d-inline-block col-lg-3 line-15">
+                  <b>Director: </b>
+                </div>
+                <div className="col d-inline-block col-lg-9 pad-left-15 line-15">
+                  {data.director}
+                </div>
+              </div>
+              <div className="mt-1"></div>
+              <div className="row">
+                <div className="col d-inline-block col-lg-2 line-15">
+                  <b>Casts: </b>
+                </div>
+                <div className="col d-inline-block col-lg-10 pad-left-15 line-15">
+                  {data.casts}
+                </div>
+              </div>
+              <div className="mt-1"></div>
+              <div className="row">
+                <div className="col d-inline-block col-lg-2 line-15">
+                  <b>Genre: </b>
+                </div>
+                <div className="col d-inline-block col-lg-10 pad-left-20 line-15">
+                  {data.genre}
+                </div>
+              </div>
             </div>
           </footer>
         </React.Fragment>
